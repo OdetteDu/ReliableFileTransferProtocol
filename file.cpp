040c9eb062824d513a0a26aa3be897ae122d1c05
yanfeiwu <Yanfei.Wu@rice.edu>
2013-11-11 22:15:07 -0600

    modify file.cpp

--- file.cpp
+++ file.cpp
@@ -5,23 +5,24 @@
 
 using namespace std;
 
+char *contents = (char*) malloc(sizeof(char)*1024*1024); //should be global
 
-void save(string filename, bool largeFile, bool lastPacket, unsigned int length, unsigned int packetIndex, char *payload[])
+void save(string filename, bool largeFile, bool lastPacket, unsigned int length, unsigned int packetIndex, char *payload)
 {
 	if(!largeFile)
 	{
-		char *contents[] = malloc(sizeof(char)*1024*1024);//should be global
-
+		memcpy(content + 2048 * packetIndex, payload, length);
+		/*
 		for(int i=0; i<length; i++)
 		{
 			contents[2048*packetIndex+i] = payload[i];
-		}
+		}*/
 
 		if(lastPacket)
 		{
 			ofstream fs;
 			fs.open(filename+".recv");
-			fs << contents;
+			fs.write(contents, 2048 * packetIndex + length);
 			fs.close();
 		}
 	}
